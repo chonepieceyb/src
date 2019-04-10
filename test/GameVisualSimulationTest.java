d33dc059bdca1a4eada274eec773f0c7306df72c
chonepieceyb <34810016+chonepieceyb@users.noreply.github.com>
2019-04-10 16:12:49 +0800

    first commit

---
+++ test/GameVisualSimulationTest.java
@@ -0,0 +1,75 @@
+ /*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import ai.core.AI;
+import ai.*;
+import ai.abstraction.HeavyRush;
+import ai.abstraction.LightRush;
+import ai.abstraction.WorkerRush;
+import ai.abstraction.WorkerRushPlusPlus;
+import ai.abstraction.cRush.CRush_V1;
+import ai.abstraction.pathfinding.BFSPathFinding;
+import ai.mcts.naivemcts.NaiveMCTS;
+import ai.scv.SCV;
+import gui.PhysicalGameStatePanel;
+import java.io.OutputStreamWriter;
+import javax.swing.JFrame;
+import myrtsai.MyRtsAi;
+import rts.GameState;
+import rts.PhysicalGameState;
+import rts.PlayerAction;
+import rts.units.UnitTypeTable;
+import util.XMLWriter;
+
+/**
+ *
+ * @author santi
+ */
+public class GameVisualSimulationTest {
+    public static void main(String args[]) throws Exception {
+        UnitTypeTable utt = new UnitTypeTable();
+        BFSPathFinding aps= new BFSPathFinding();
+        PhysicalGameState pgs = PhysicalGameState.load("maps/16x16/basesWorkers16x16.xml", utt);
+//        PhysicalGameState pgs = MapGenerator.basesWorkers8x8Obstacle();
+
+        GameState gs = new GameState(pgs, utt);
+        int MAXCYCLES = 5000;
+        int PERIOD = 20;
+        boolean gameover = false;
+        
+        AI ai1 = new MyRtsAi(utt,aps);        
+        AI ai2 = new CRush_V1(utt,aps);
+       // AI ai2 = new RandomBiasedAI();
+
+        JFrame w = PhysicalGameStatePanel.newVisualizer(gs,640,640,false,PhysicalGameStatePanel.COLORSCHEME_BLACK);
+//        JFrame w = PhysicalGameStatePanel.newVisualizer(gs,640,640,false,PhysicalGameStatePanel.COLORSCHEME_WHITE);
+
+        long nextTimeToUpdate = System.currentTimeMillis() + PERIOD;
+        do{
+            if (System.currentTimeMillis()>=nextTimeToUpdate) {
+                PlayerAction pa1 = ai1.getAction(0, gs);
+                PlayerAction pa2 = ai2.getAction(1, gs);
+                gs.issueSafe(pa1);
+                gs.issueSafe(pa2);
+
+                // simulate:
+                gameover = gs.cycle();
+                w.repaint();
+                nextTimeToUpdate+=PERIOD;
+            } else {
+                try {
+                    Thread.sleep(1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }while(!gameover && gs.getTime()<MAXCYCLES);
+        ai1.gameOver(gs.winner());
+        ai2.gameOver(gs.winner());
+        
+        System.out.println("Game Over");
+    }    
+}
